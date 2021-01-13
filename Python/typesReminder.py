ca5bc04f6e63997a7fbce2f18743a61de764982d
ksalmond <krsalmond@gmail.com>
2021-01-13 15:43:34 +0000

    Added folder this time...

---
+++ Python/typesReminder.py
@@ -0,0 +1,23 @@
+""" Reminder file for some basic python types"""
+
+"""string"""
+name = "Aaron"
+print(name)
+print(type(name))
+
+"""integer"""
+number = 55
+print (number)
+print(type(number))
+
+"""boolean"""
+myBool = True
+print(myBool)
+print(type(myBool))
+
+"""list"""
+mylist = ["hello", "cruel", "world"]
+print(mylist)
+print(type(mylist))
+
+
